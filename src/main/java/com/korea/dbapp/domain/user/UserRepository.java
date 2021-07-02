b820bcf2b28f595eebf3e53c91845c08f261dffe
Your Name <you@example.com>
2021-07-02 16:25:13 +0900

    1. ORM 매핑 시작

---
+++ src/main/java/com/korea/dbapp/domain/user/UserRepository.java
@@ -0,0 +1,16 @@
+package com.korea.dbapp.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	@Query(value = "SELECT * FROM user WHERE username = :username",nativeQuery = true)
+	User mfindByUsername(String username);
+	
+	@Query(value = "SELECT * FROM user WHERE username = :username AND password = :password", nativeQuery = true)
+	User mLogin(String username, String password);
+	
+}
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<div class="jumbotron text-center" style="margin-bottom: 0">
+	<p>🌿 코리아 it 센터</p>
+	<p>🏴010 2333 xxxx</p>
+	<p>🚨613-2222-483xx</p>
+</div>
+
+</body>
+</html>
